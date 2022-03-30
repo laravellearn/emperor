d697380c7554bf42ca9b6481bd0485db9d098baf
Laravel Learn <info@laravellearn.ir>
2022-03-30 14:19:30 +0430

    Episode 10

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,14 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function dbUsers(){
+        $db = DB::connection('mysql-users')->table('users')->get();
+        dd($db);
+    }
 }
