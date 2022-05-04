b8188d8393a8b625268c687a045451a2e3d23b47
Laravel Learn <info@laravellearn.ir>
2022-05-04 13:02:48 +0430

    Episode 56

--- resources/views/livewire/admin/roles/index.blade.php
+++ resources/views/livewire/admin/roles/index.blade.php
@@ -1,3 +1,98 @@
+@section('title', 'نقش ها')
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('admin/plugins/select2/css/select2.min.css') }}">
+@endsection
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
+    <div class="main-content">
+        <div class="data-table-area">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xl-4 box-margin height-card">
+                        <div class="card card-body">
+                            <h4 class="card-title">افزودن نقش</h4>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-12 col-xs-12">
+                                    <form>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail111">عنوان نقش(لاتین):</label>
+                                            <input type="text" class="form-control" id="exampleInputEmail111">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail12">توضیحات نقش(فارسی):</label>
+                                            <input type="text" class="form-control" id="exampleInputEmail111">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail12">سطح دسترسی ها:</label>
+                                            <select class="js-example-basic-single form-control" multiple="multiple"
+                                                name="" style="width: 100%;">
+                                                <option value="AL">Alabama</option>
+                                            </select>
+                                        </div>
+                                        <button type="submit" class="btn btn-outline-success mb-2 mr-2"
+                                            style="float:left;"><i class="fa fa-save"></i> ذخیره</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-lg-8 box-margin">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title mb-2">لیست نقش ها</h4>
+                                <button type="button" class="btn btn-danger mb-2 mr-2"
+                                    style="float:left;margin-top:-37px;"><i class="fa fa-refresh"></i> سطل
+                                    زباله</button>
+                                <button type="button" class="btn btn-primary mb-2 mr-2"
+                                    style="float:left;margin-top:-37px;"><i class="fa fa-file-excel-o"></i> خروجی
+                                    اکسل</button>
+                                <hr>
+                                <input wire:model="search" type="search" class="form-control mb-2 w-50 float-left"
+                                    placeholder="جستجو...">
+
+                                <table id="datatable-buttons" class="table table-striped dt-responsive nowrap"
+                                    style="width:104%">
+                                    <thead>
+                                        <tr>
+                                            <th>عنوان نقش</th>
+                                            <th>توضیحات</th>
+                                            <th>سطوح دسترسی</th>
+                                            <th>عملیات</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        <tr>
+                                            <td>نام کاربری</td>
+                                            <td>سیستم</td>
+                                            <td>سطوح دسترسی</td>
+                                            <td>
+                                                <a href="javascript:void(0);" class="action-icon"> <i
+                                                        class="zmdi zmdi-edit zmdi-custom"></i></a>
+                                                <a href="javascript:void(0);" class="action-icon"> <i
+                                                        class="zmdi zmdi-delete zmdi-custom"></i></a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+
+                            </div> <!-- end card body-->
+                        </div> <!-- end card -->
+                    </div><!-- end col-->
+                </div>
+                <!-- end row-->
+
+            </div>
+        </div>
+    </div>
+    @section('scripts')
+        <script src="{{ asset('admin/plugins/select2/js/select2.full.min.js') }}"></script>
+
+        <script>
+            $(document).ready(function() {
+                $('.js-example-basic-single').select2();
+            });
+        </script>
+    @endsection
 </div>
